3f91b565f8418cbb06980c8a7d59e8156dfea3ab
sasa2031 <salina31@web.de>
2021-05-30 16:06:30 +0200

    Aufgabe_3.1

--- Aufgabe_3/Aufgabe_3.1/script.js
+++ Aufgabe_3/Aufgabe_3.1/script.js
@@ -1,11 +1,17 @@
 "use strict";
+//Aufgabe 2
 async function Data(_pURL) {
     let response = await fetch(_pURL);
     console.log("Response", response);
-    let formData = await response.formData();
-    console.log(formData.get("fname"));
-    console.log(formData.get("lname"));
-    console.log(formData.get("email"));
+    let formData = new FormData(document.forms[0]); //neues Formelement anlegen und mit 1. Form befüllen
+    //Daten per GET mit fetch senden 
+    let url = "https://salinasapp.herokuapp.com";
+    let query = new URLSearchParams(formData);
+    url = url + "?" + query.toString(); //query an url anfügen
+    await fetch(url);
+    formData.get("fname");
+    formData.get("lname");
+    formData.get("email");
 }
-Data("https://gis-example.herokuapp.com"); //Link App und Github
+Data("https://salinasapp.herokuapp.com");
 //# sourceMappingURL=script.js.map
--- Aufgabe_3/Aufgabe_3.1/script.ts
+++ Aufgabe_3/Aufgabe_3.1/script.ts
@@ -1,14 +1,21 @@
- 
 
-    async function Data(_pURL: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData: FormData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("lname"));
-        console.log(formData.get("email"));
-    }
-   
-    Data("https://gis-example.herokuapp.com"); //Link App und Github
+//Aufgabe 2
+async function Data(_pURL: RequestInfo): Promise<void> {
+    let response: Response = await fetch(_pURL);
+    console.log("Response", response);
+    let formData: FormData = new FormData(document.forms[0]); //neues Formelement anlegen und mit 1. Form befüllen
 
-   
+    //Daten per GET mit fetch senden 
+    let url: string = "https://salinasapp.herokuapp.com";
+    let query: URLSearchParams = new URLSearchParams(<any>formData); 
+    url = url + "?" + query.toString(); //query an url anfügen
+    await fetch(url);
+
+    formData.get("fname");
+    formData.get("lname");
+    formData.get("email");
+}
+
+Data("https://salinasapp.herokuapp.com");
+
+
